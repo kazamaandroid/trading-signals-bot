147a77e8d31e60463c961d42b4cef245729c29ba
kazamaandroid <kazamaandroid@gmail.com>
2025-05-22 01:41:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import os
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+# Aquí tu código que use esas variables
