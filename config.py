3b7be9e6362eddc3dcbd77bcf5434f44b1b42c7e
kazamaandroid <kazamaandroid@gmail.com>
2025-05-22 01:30:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# config.py
+import os
 
-# Token del bot de Telegram
-TELEGRAM_BOT_TOKEN = "6498133160:AAG-fxkpMSzYmznHvbVbdpCmS4RgFjQxOQo"
-
-# ID del chat de Telegram (canal o grupo)
-TELEGRAM_CHAT_ID = "-1002081453292"
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
